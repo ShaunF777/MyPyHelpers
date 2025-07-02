4ffa69e875d21e54a3516d95b35250b9c83bf4cf
ShaunF777 <shaunfr777@gmail.com>
2025-07-02 13:12:28 +0200

    refactor QR code generation to accept command line input

--- QRcodeMaker.py
+++ QRcodeMaker.py
@@ -1,4 +1,10 @@
 import qrcode
+import sys
 
-img = qrcode.make ("https://youtu.be/6rmErwU5E-k")
-img.save("SolderingQRcode.png")
+if len(sys.argv) > 1:
+    data = sys.argv[1] # Get data from command line argument
+    img = qrcode.make(data)  # Create QR code image
+    img.save("QRcode.png")  # Save the image to a file
+else:
+    print("Usage: python QRcodeMaker.py <data>")
+    sys.exit(1)
